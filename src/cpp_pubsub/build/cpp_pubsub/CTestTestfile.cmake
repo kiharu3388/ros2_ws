b2caaf068f7c204f31df0e93a782d50edf586016
Haruka Kita <138560011+kiharu3388@users.noreply.github.com>
2024-11-18 15:13:59 +0900

    Delete

--- src/cpp_pubsub/build/cpp_pubsub/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/ros2_ws/src/cpp_pubsub
-# Build directory: /root/ros2_ws/src/cpp_pubsub/build/cpp_pubsub
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
